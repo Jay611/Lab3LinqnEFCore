3b68e25b84d59ca1dd9aa99c81dd0f42df0c61b2
Jay611 <nanbaejjae@gmail.com>
2020-03-14 17:57:57 -0400

    20200314 Grouping DataGrid

--- Question3/MainWindow.xaml.cs
+++ Question3/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,7 +37,7 @@ namespace Question3
             dbContext.AuthorIsbn.Load();
             titlesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("titlesViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
-            //titlesViewSource.Source = dbContext.Titles.Local.ToObservableCollection();
+            titlesViewSource.Source = dbContext.Titles.Local.ToObservableCollection();
         }
 
         private void SortByTitleButton_Click(object sender, RoutedEventArgs e)
@@ -57,8 +58,8 @@ namespace Question3
                          select new
                          {
                              AuthorId = a.AuthorId,
-                             Firstname = a.FirstName,
-                             Lastname = a.LastName
+                             FirstName = a.FirstName,
+                             LastName = a.LastName
                          };
             var TitleAndAuthor = from t in title
                                  join ai in authorIsbn on t.ISBN equals ai.ISBN
@@ -67,9 +68,10 @@ namespace Question3
                                  select new
                                  {
                                      Title = t.Title,
-                                     AuthorFirstname = a.Firstname,
-                                     AuthorLastname = a.Lastname
+                                     FirstName = a.FirstName,
+                                     LastName = a.LastName
                                  };
+
             titlesViewSource.Source = TitleAndAuthor;
         }
 
@@ -91,21 +93,94 @@ namespace Question3
                          select new
                          {
                              AuthorId = a.AuthorId,
-                             Firstname = a.FirstName,
-                             Lastname = a.LastName
+                             FirstName = a.FirstName,
+                             LastName = a.LastName
                          };
             var TitleAndAuthor = from t in title
                                  join ai in authorIsbn on t.ISBN equals ai.ISBN
                                  join a in author on ai.AuthorId equals a.AuthorId
-                                 orderby t.Title, a.Lastname, a.Firstname
+                                 orderby t.Title, a.LastName, a.FirstName
                                  select new
                                  {
                                      Title = t.Title,
-                                     AuthorFirstname = a.Firstname,
-                                     AuthorLastname = a.Lastname
+                                     FirstName = a.FirstName,
+                                     LastName = a.LastName
                                  };
+
             titlesViewSource.Source = TitleAndAuthor;
         }
-        
+
+        private void GroupByTitleAndAuthorButton_Click(object sender, RoutedEventArgs e)
+        {
+            var title = from t in dbContext.Titles.Local
+                        select new
+                        {
+                            Title = t.Title,
+                            ISBN = t.Isbn
+                        };
+            var authorIsbn = from ai in dbContext.AuthorIsbn.Local
+                             select new
+                             {
+                                 ISBN = ai.Isbn,
+                                 AuthorId = ai.AuthorId
+                             };
+            var author = from a in dbContext.Authors.Local
+                         select new
+                         {
+                             AuthorId = a.AuthorId,
+                             FirstName = a.FirstName,
+                             LastName = a.LastName
+                         };
+            var TitleAndAuthor = from t in title
+                                 join ai in authorIsbn on t.ISBN equals ai.ISBN
+                                 join a in author on ai.AuthorId equals a.AuthorId
+                                 select new
+                                 {
+                                     Title = t.Title,
+                                     FirstName = a.FirstName,
+                                     LastName = a.LastName
+                                 };
+            var grouped = TitleAndAuthor.GroupBy(item => item.Title).OrderBy(g => g.Key)
+                .Select(g => new { key = g.Key, Authores = g.OrderBy(x=>x.LastName).ThenBy(g1=>g1.FirstName)})
+                .SelectMany(g=>g.Authores);
+
+            titlesViewSource.Source = grouped;
+        }
+
+        // not for assignment
+        private void GroupingDataGridButton_Click(object sender, RoutedEventArgs e)
+        {
+            var title = from t in dbContext.Titles.Local
+                        select new
+                        {
+                            Title = t.Title,
+                            ISBN = t.Isbn
+                        };
+            var authorIsbn = from ai in dbContext.AuthorIsbn.Local
+                             select new
+                             {
+                                 ISBN = ai.Isbn,
+                                 AuthorId = ai.AuthorId
+                             };
+            var author = from a in dbContext.Authors.Local
+                         select new
+                         {
+                             AuthorId = a.AuthorId,
+                             FirstName = a.FirstName,
+                             LastName = a.LastName
+                         };
+            var TitleAndAuthor = from t in title
+                                 join ai in authorIsbn on t.ISBN equals ai.ISBN
+                                 join a in author on ai.AuthorId equals a.AuthorId
+                                 select new
+                                 {
+                                     Title = t.Title,
+                                     FirstName = a.FirstName,
+                                     LastName = a.LastName
+                                 };
+
+            Window1 GroupByTitleWindow = new Window1(TitleAndAuthor);
+            GroupByTitleWindow.Show();
+        }        
     }
 }
---
+++ Question3/Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Question3
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private IEnumerable<object> titleAndAuthor;
+
+        public Window1(IEnumerable<object> titleAndAuthor)
+        {
+            InitializeComponent();
+            this.titleAndAuthor = titleAndAuthor;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            //System.Windows.Data.CollectionViewSource authorsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("authorsViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+
+            ListCollectionView collection = new ListCollectionView(titleAndAuthor.ToList());
+            collection.GroupDescriptions.Add(new PropertyGroupDescription("Title"));
+            authorsDataGrid.ItemsSource = collection;
+        }
+    }
+}
